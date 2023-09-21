1c011311d2bc6da8c4870153f70deca6ae773b78
Joseph Nguyen <theantisine@gmail.com>
2023-09-21 16:50:14 -0700

    Working release

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import discord, json, requests, urllib.parse
+import discord, json, requests, asyncio
+from discord import app_commands
 
 with open('config.json', 'r') as f:
     data = json.load(f)
@@ -16,22 +17,24 @@ class pre_bot(discord.Client):
         print("Bot Online")
         
 bot = pre_bot()
-tree = discord.app_commands.CommandTree(bot)
+tree = app_commands.CommandTree(bot)
 
-@tree.command(name="search", description="Search for an anime scene")
+@tree.command(name="search", description="Search for an anime scene", guild=discord.Object(id=1121639089897230516)) # ! Test server
 async def self(interaction: discord.Interaction, url:str):
-    # payload = requests.get("https://api.trace.moe/search?url={}".format(urllib.parse.quote_plus(url))).json()
     try:
-        payload = requests.get("https://api.trace.moe/search?anilistInfo&url={}".format(urllib.parse.quote_plus(url))).json()
+        payload = requests.get(f"https://api.trace.moe/search?anilistInfo&url={url}").json()
         title = payload["result"][0]["anilist"]["title"]["romaji"]
     except:
-        payload = requests.get("https://api.trace.moe/search?url={}".format(urllib.parse.quote_plus(url))).json()
+        payload = requests.get(f"https://api.trace.moe/search?url={url}").json()
         title = (payload["result"][0]["filename"]).replace('.mp4', '')
     time_stamp, image_link = payload["result"][0]["from"], payload["result"][0]["image"]
-    # title, time_stamp, image_link = (payload["result"][0]["filename"]).replace('.mp4', ''), payload["result"][0]["from"], payload["result"][0]["image"]
-    # requests.post("https://api.trace.moe/search", data=open("demo.jpg", "rb"),headers={"Content-Type": "image/jpeg"}).json() # Upload version
     
     embed = discord.Embed(title=f"{title}", description=f"{time_stamp}", color=discord.Colour.green())
     embed.set_image(url=image_link)
     
-    await interaction.response.reply(embed=embed)
+    await interaction.response.defer()
+    await asyncio.sleep(10) 
+    await interaction.followup.send(embed=embed, ephemeral=True)
+        
+if __name__ == '__main__':
+    bot.run(TOKEN)
